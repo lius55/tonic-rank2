94d7dead47f5f6bcf8d4830f907873b165aadfc1
tony <tony56recruit@gmail.com>
2017-08-17 17:25:19 +0900

    指摘修正

--- api/goods-info.php
+++ api/goods-info.php
@@ -27,12 +27,28 @@ while($row = $stmt->fetch(PDO::FETCH_OBJ)) {
 	$response->goodsInfo->assessList[] = $row;
 }
 
-// 成分の取得
-$stmt = $dbh->prepare("select name,detail from TRN_GOODS_COMP,TRN_COMP where goods_id = :id and TRN_COMP.id = TRN_GOODS_COMP.comp_id");
-$stmt->bindParam(':id', $goodsId);
-$stmt->execute();
+$compDefList = array();
+$compDefList[] = array("effect"=>"男性ホルモン抑制", "column"=>"hormone_supp");
+$compDefList[] = array("effect"=>"血行促進", "column"=>"circulation_promot");
+$compDefList[] = array("effect"=>"毛母細胞活性化", "column"=>"cell_activation");
+$compDefList[] = array("effect"=>"バジル活性化", "column"=>"bulge_activation");
+$compDefList[] = array("effect"=>"成長促進", "column"=>"growth_promot");
+$compDefList[] = array("effect"=>"保湿", "column"=>"moist");
+$compDefList[] = array("effect"=>"抗酸化・殺菌", "column"=>"anti_virus");
 
-while($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+foreach ($compDefList as $comp) {
+	// echo "comp=" . $comp["effect"] . " " . $comp["column"];
+	// 成分の取得
+	$stmt = $dbh->prepare("select name from TRN_GOODS_COMP,TRN_COMP where goods_id=:id and TRN_COMP.id = TRN_GOODS_COMP.comp_id and " . $comp["column"] . ">0");
+	$stmt->bindParam(":id", $goodsId);
+	$stmt->execute();
+
+	$row = new stdClass();
+	$row->effect = $comp["effect"];
+	$row->compName = array();
+	while($compName = $stmt->fetch(PDO::FETCH_OBJ)) {
+		$row->compName[] = $compName->name;
+	}
 	$response->goodsInfo->compList[] = $row;
 }
 
